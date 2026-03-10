a54f88cd0ca1a79cea43555ad67e389018874b1e
Junaid Khattak <junaid@doerz.tech>
2026-03-10 21:50:08 +0500

    updated x-frame headers

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,17 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Content-Security-Policy", value: "frame-ancestors 'none'" },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
